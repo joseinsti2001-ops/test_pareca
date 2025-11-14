a1c82fbb8524fe606e6231b5e96c2dc228f29212
joseinsti2001-ops <jose.insti2001@gmail.com>
2025-11-14 19:07:15 +0100

    Update fmt.Println to print 'Goodbye World'

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Intercambiamos elementos
+        [array[i], array[j]] = [array[j], array[i]]; // Intercambiamos elementos 
     }
 }
 
@@ -669,6 +669,7 @@ let currentQuestion = 0;
 let score = 0;
 
 // --- FIN DEL SCRIPT ---
+
 
 
 
